e135ad12b9118ba1b53243755ce104f7536f4fc4
Robert <robert.p1991@gmail.com>
2018-03-17 18:15:49 +0100

    add startup model and and add crawlers

--- model/crawler/controller.js
+++ model/crawler/controller.js
@@ -1,10 +1,40 @@
 const Controller = require('../../lib/controller')
 const crawlerFacade = require('./facade')
+// const crawler = require('crawler')
+// const phantom = require('phantom')
+const puppeteer = require('puppeteer')
+
+
+
+const angelUrl = 'https://angel.co/companies'
 
 class CrawlerController {
    
-   crawlSite(req, res, next) {
-      res.status(200).json(req.params)
+   crawlSite(req, response, next) {
+      puppeteer.launch({args: ['--no-sandbox', '--ignore-certificate-errors', '--ignore-certificate-errors-spki-list']}).then(browser => {
+         browser.newPage().then(page => {
+            page.goto(angelUrl).then(() => {
+               console.log('here!!')
+               page.evaluate(() => {
+                  console.log('evaluate')
+                  // console.log(document.querySelector('.npm-expansions').textContent)
+               }).then(() => {
+                  console.log('evaluate')
+               })
+            }).catch((err) => {
+               console.log(err)   
+            })
+            page.close().then(() => {
+               browser.close()
+            }).catch((err) => {
+               console.log(err)
+            })
+         }).catch((err) => {
+            console.log(err)
+         })
+      }).catch((err) => {
+         console.log(err)
+      })
    }
 }
 
--- model/startup/schema.js
+++ model/startup/schema.js
@@ -1,9 +1,30 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const team = new Schema({
+  name: {type: String},
+  position: {type: String},
+  age: {type: Number}
+})
+
 const startupSchema = new Schema({
-  title: { type: String, required: true },
-  body: { type: String }
+  name: {type: String},
+  link: {type: String},
+  logo: {type: String},
+  description: {type: String},
+  team: [team],
+  dataHost: {type: Boolean},
+  technology: {type: String},
+  sector: {type: String},
+  age: {type: Number},
+  country: {type: String},
+  city: {type: String},
+  solution: {type: String},
+  category: {type: String},
+  patents: {type: String},
+  partnerships: {type: Number},
+  pocs: {type: Number},
+  fundingRound: {type: String}
 })
 
 module.exports = startupSchema
